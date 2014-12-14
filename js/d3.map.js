33359244e50a456894e64a7e96a8521ac66cd32f
digitclue <digitclue@gmail.com>
2014-12-14 20:06:14 +0200

    Start using geojson Split init function Add resize handler

--- js/d3.map.js
+++ js/d3.map.js
@@ -5,30 +5,88 @@ bindReady(function(){
 
 function initMap(){
 	// get svg element
-	var map = d3.select(".map"),
-		group = map.append('g');
+	d3.selectAll('.map').each(function(d, index){
+		var map = d3.select(this),
+			width = parseInt(map.style('width')),
+			height = parseInt(map.style('height')),
+			projection = d3.geo.mercator(),
+			path = d3.geo.path().projection(projection),
+			mapBounds,
+			timer;
 
-	group
-		.attr('transform', 'translate(0, 525) scale(0.1,-0.1)')
-		.attr('width', 500)
-		.attr('height', 500);
-	
-	//load data from json
-	d3.json('inc/map.json', function(error, data){
-		if (error){
-			throw error;
-		} else {
-			group.selectAll('path')
-				.data(data)
+
+		//load data from json
+		d3.json('inc/regions.geojson', function(error, data){
+			if (error){
+				throw error;
+			} else {
+				drawMap(data);
+				attachEvents();
+			}
+		});
+
+		function drawMap(data){
+			mapBounds = getMapBounds(data);
+
+			var groups = map.selectAll('.group')
+				.data(data.features)
 				.enter()
-				.append('path')
-				.attr('d', function(data){
-					return data.path;
+				.append('g')
+				.classed('group', true);
+
+			groups.append('path').classed('area', true);
+
+			resizeMap();
+		}
+
+		function getMapBounds(data){
+			projection.scale(1).translate([0, 0]);
+			return path.bounds(data);
+		}
+
+		function resizeMap(){
+			var scale, translate;
+
+			width = parseInt(map.style('width'));
+			height = parseInt(map.style('height'));
+
+			scale = 1 / Math.max((mapBounds[1][0] - mapBounds[0][0]) / width, (mapBounds[1][1] - mapBounds[0][1]) / height);
+			translate = [(width - scale * (mapBounds[1][0] + mapBounds[0][0])) / 2, (height - scale * (mapBounds[1][1] + mapBounds[0][1])) / 2];
+
+			projection
+				.scale(scale)
+				.translate(translate);
+
+			map.selectAll('.area').attr('d', path);
+		}
+
+		function resizeHandler(){
+			if (timer) clearTimeout(timer);
+			timer = setTimeout(function(){
+				resizeMap();
+			}, 100);
+		}
+
+		function attachEvents(){
+			var initColor = map.select('.area').style('fill');
+
+			d3.select(window).on('resize.' + index, resizeHandler);
+
+			map.selectAll('.area')
+				.on('mouseenter', function(data){
+					d3.select(this)
+						.transition()
+						.duration(400)
+						.style('fill', '#10e0e0');
 				})
-				.attr('fill', function(data){
-					return data.color;
+				.on('mouseleave', function(data){
+					d3.select(this)
+						.transition()
+						.duration(400)
+						.style('fill', initColor);
 				});
 		}
+		
 	});
 }
 
